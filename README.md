dbe3879ba314283aaa9821dc942bb31cd4bd8cb0
Camilo Romero <militoromero10@gmail.com>
2020-07-21 17:22:50 -0500

    command line runner and codecov

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # spring5-mvc-rest
-[![CircleCI](https://circleci.com/gh/militoromero10/spring5-mvc-rest.svg?style=svg)](https://circleci.com/gh/militoromero10/spring5-mvc-rest)
 [![CircleCI](https://circleci.com/gh/militoromero10/spring5-mvc-rest/tree/develop.svg?style=svg)](https://circleci.com/gh/militoromero10/spring5-mvc-rest/tree/develop)
+[![codecov](https://codecov.io/gh/militoromero10/spring5-mvc-rest/branch/develop/graph/badge.svg)](https://codecov.io/gh/militoromero10/spring5-mvc-rest)
 # Spring Framework 5 MVC Rest Application
 #This repository is for an example application built in my Spring Framework 5
---
+++ src/main/java/com/milo/rest/boostrap/Bootstrap.java
@@ -0,0 +1,43 @@
+package com.milo.rest.boostrap;
+
+import com.milo.rest.domain.Category;
+import com.milo.rest.respository.CategoryRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Bootstrap implements CommandLineRunner {
+
+    private CategoryRepository categoryRepository;
+
+    public Bootstrap(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Category fruits = new Category();
+        fruits.setName("Fruits");
+
+        Category dried = new Category();
+        fruits.setName("Dried");
+
+        Category fresh = new Category();
+        fruits.setName("Fresh");
+
+        Category exotic = new Category();
+        fruits.setName("Exotic");
+
+        Category nuts = new Category();
+        fruits.setName("Nuts");
+
+        categoryRepository.save(fruits);
+        categoryRepository.save(dried);
+        categoryRepository.save(fresh);
+        categoryRepository.save(exotic);
+        categoryRepository.save(nuts);
+
+        System.out.println("Data loaded = " + categoryRepository.count());
+    }
+}
